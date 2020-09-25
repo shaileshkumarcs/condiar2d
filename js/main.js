658d6df643679bbef796ffdc0eef6d2835788fd8
Shailesh Kumar <shailesh.kumarcs@gmail.com>
2020-09-25 12:37:02 +0530

    Conflict and new code added

--- js/main.js
+++ js/main.js
@@ -13,11 +13,10 @@ const data = {
 socket.emit('initialConditionBySocket', team_id, data);
 
 socket.on('receive_initialConditionBySocket', function(initialData){
+
 	setInitialConditionToAll(initialData)
-	//console.log("MSG", initialData);
-	//console.log("Cash", initialData.Cash);
-	//document.getElementById("cash_value").innerHTML = initialData.Cash;
-	//document.getElementById("trade_receivable_value").innerHTML = initialData.Trade_receivables;
+	console.log("MSG", initialData);
+	console.log("Cash", initialData.Cash);
 
 });
 
@@ -25,6 +24,27 @@ socket.on('receive_initialConditionBySocket', function(initialData){
 function setInitialConditionToAll(initialData){
 	console.log('initialData');
 	console.log(initialData);
+	console.log("MSG", initialData);
+	console.log("Cash", initialData.Cash);
+	document.getElementById("cash_value").innerHTML = initialData.Cash;
+	document.getElementById("trade_receivable_value").innerHTML = initialData.Trade_receivables;
+	document.getElementById("material_inventory").innerHTML = initialData.Inventory_materials;
+	document.getElementById("material_inventory").innerHTML = initialData.Inventory_materials;
+	document.getElementById("Property_Plant_and_Equipment_Plant_value").innerHTML = initialData.Property_Plant_and_Equipment_Plant_value;
+	document.getElementById("Intangible_assets_Goodwill").innerHTML = initialData.Intangible_assets_Goodwill;
+	document.getElementById("Short_term_liabilities").innerHTML = initialData.Short_term_liabilities;
+	document.getElementById("Long_term_liabilities").innerHTML = initialData.Long_term_liabilities_3_Year;
+	document.getElementById("Other_liabilities").innerHTML = initialData.Other_liabilities;
+	document.getElementById("Share_Capital").innerHTML = initialData.Share_Capital;
+	document.getElementById("Reserves").innerHTML = initialData.Reserves;
+
+	document.getElementById("financial_short_term_libabilities_value").innerHTML = initialData.Short_term_liabilities;
+	document.getElementById("financial_short_term_libabilities_percent").innerHTML = initialData.Short_term_loan_interest_rate+'%';
+	document.getElementById("financial_long_term_libabilities_value").innerHTML = initialData.Long_term_liabilities_3_Year;
+	document.getElementById("financial_long_term_libabilities_percent").innerHTML = initialData.Long_term_loan_interest_rate+'%';
+	// document.getElementById("Share_Capital_financial").innerHTML = initialData.
+	// document.getElementById("").innerHTML = initialData.
+	// document.getElementById("").innerHTML = initialData.
 
 }
 
